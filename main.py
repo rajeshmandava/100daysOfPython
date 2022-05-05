cde3e9584e490fa79976fc6be7956168a37978a4
Rajesh Mandava <rajesh.mandava07@gmail.com>
2022-05-05 21:18:09 +0200

    PEMDAS

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print('This is Hello main.py')
+print_val = "This is print statement"
+print(print_val)
+#leaving a comment in code
+
+#arithmetic operations in a line of code
+#PEMDAS Left to Right
+#Paranthesis ()
+#Exponenets **
+#Multiplication *
+#Division /
+#addition +
+#substraction -
+print(3*3+3/3-3)
