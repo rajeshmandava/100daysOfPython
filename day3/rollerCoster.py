e7a85d216a17cda5a3537c085630134834bd1ea9
Rajesh Mandava <rajesh.mandava07@gmail.com>
2022-05-07 00:21:04 +0200

    bill feature added

--- day3/rollerCoster.py
+++ day3/rollerCoster.py
@@ -1,12 +1,20 @@
 height = int(input("Enter your height in cms:"))
-age = int(input("Enter your age:"))
+
+bill = 0
 if height >= 120:
   print("allowed to ride rollercoster")
+  age = int(input("Enter your age:"))
   if(age<12):
-    print("Pay $5")
+    bill = 5
   elif(age<=18):
-    print("pay $7")
+    bill = 7
   else:
-    print("pay $10")
+    bill = 12
+  wants_photo = input("Do you want photo taken? Y or N.")
+  if wants_photo=="Y":
+    bill +=3
+  print(f"your final bill is {bill}")
+
+
 else:
   print("Not allowed to ride rollercoster")
