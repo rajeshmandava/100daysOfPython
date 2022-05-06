fa46ab85a474a9f12ba60c9a8e2ef9b003cd8d92
Rajesh Mandava <rajesh.mandava07@gmail.com>
2022-05-07 00:39:47 +0200

    roller coster feature added

--- day3/rollerCoster.py
+++ day3/rollerCoster.py
@@ -8,10 +8,13 @@ if height >= 120:
     bill = 5
   elif(age<=18):
     bill = 7
+  elif age>=45 and age <=55:
+    print("Everything is going to be okay, Have a free ride on us!")
   else:
     bill = 12
+  
   wants_photo = input("Do you want photo taken? Y or N.")
-  if wants_photo=="Y":
+  if wants_photo=="Y" and (age<45 and age>55):
     bill +=3
   print(f"your final bill is {bill}")
 
