815c27cd9b73322a384832fbdb0c1153d3c38071
Rajesh Mandava <rajesh.mandava07@gmail.com>
2022-05-09 22:33:34 +0200

    treasure map using list indexers

---
+++ day4/treasure_map.py
@@ -0,0 +1,17 @@
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+int_pos = int(position)
+x=int_pos%10
+y=int(int_pos/10)
+map[x-1][y-1]="x"
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
