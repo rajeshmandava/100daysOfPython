a5a6455c180f0aecee990526483e27d24ceba3ed
Rajesh Mandava <rajesh.mandava07@gmail.com>
2022-05-09 23:27:29 +0200

    Rock Paper Scissor game

---
+++ day4/rock_paper_scissor.py
@@ -0,0 +1,48 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+choice = int(input('''Enter your choice
+0 for Rock
+1 for Scissors
+2 for Paper'''))
+
+outcome = [rock,paper,scissors]
+printer = ["Rock", "Paper", "Scissors"]
+
+comp_choice = random.randint(0,2)
+print(f"computer choose {printer[comp_choice]} '\n{outcome[comp_choice]}")
+if choice == comp_choice:
+  print("Draw")
+elif choice ==0 and comp_choice==1:
+  print("You win")
+elif choice == 1 and comp_choice==2:
+  print("You win")
+elif choice == 2 and comp_choice==0:
+  print("You win")
+else:
+  print("Computer wins")
+
