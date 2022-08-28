15b8b19a0d37501c7b29aa85f36b5550511d73e3
Rajesh Mandava <rajesh.mandava07@gmail.com>
2022-08-28 17:45:12 +0200

    Introduction to function and reeborg challenges

---
+++ day6/python_function.py
@@ -0,0 +1,12 @@
+print("Hello")
+print(len("Hello"))
+
+# Creating our functions in python
+
+# Defining my_function
+def my_function():
+  print("Hello")
+  print("Bye")
+
+# Calling function
+my_function()
---
+++ day6/reeborg_challenge.py
@@ -0,0 +1,34 @@
+# Challenge 1 
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+turn_left()    
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
+
+# Challenge 2:
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+for i in range(0,6):
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
